5cfc7312bc3a56cc0f03f52dea53cbfe5a7d10a6
Bailey DeSouza <bdesouza@email.unc.edu>
2023-01-25 20:19:00 -0500

    made progress on the 'read and code' section.

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const fs = require('fs');
 
 const minimist = require('minimist');
 
-const port = require('minimist')(process.argv.slice(2));
+const args = require('minimist')(process.argv.slice(2))
 
+const port = args.port || 3000
 
-// Require minimist module (make sure you install this one via npm).
+fs.readFile('./public/index.html', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
+});
 
-// Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
